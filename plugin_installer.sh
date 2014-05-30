439ac0b71e56663a0dc35a0d19b6e9268b0258e7
Vipul Nataraj <vnataraj@purdue.edu>
2014-05-30 00:58:05 -0700

    added some more plugins

--- plugin_installer.sh
+++ plugin_installer.sh
@@ -1,6 +1,6 @@
 #plugin installer/updater for all current plugins
 home=~/Documents/dotconfigfiles
-plugins="https://github.com/ervandew/supertab.git https://github.com/scrooloose/syntastic.git https://github.com/bling/vim-airline.git https://github.com/tpope/vim-fugitive.git https://github.com/altercation/vim-colors-solarized.git https://github.com/edkolev/tmuxline.vim"
+plugins="https://github.com/ervandew/supertab.git https://github.com/scrooloose/syntastic.git https://github.com/bling/vim-airline.git https://github.com/tpope/vim-fugitive.git https://github.com/altercation/vim-colors-solarized.git https://github.com/edkolev/tmuxline.vim https://github.com/kien/ctrlp.vim.git https://github.com/scrooloose/nerdtree.git https://github.com/majutsushi/tagbar.git"
 
 #install pathogen if it doesn't exist already
 
